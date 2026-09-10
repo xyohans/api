911783e7af66e663dc40a012ff3456d95475a980
yohannis <yohannis.abrham0@gmail.com>
2026-09-10 12:06:51 +0300

    Update server log to reflect dynamic port usage

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.set('io', io);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
